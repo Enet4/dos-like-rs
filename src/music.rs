b5daacd0ebd78a6435776e7067207d669f90c7eb
Eduardo Pinho <enet4mikeenet@gmail.com>
2022-03-06 15:55:47 +0000

    Extend music module

--- src/music.rs
+++ src/music.rs
@@ -50,6 +50,34 @@ impl Music {
         }
     }
 
+    /// Loads a music from a MOD file.
+    pub fn load_mod(path: impl AsRef<str>) -> Result<Music, FileError> {
+        let filename = CString::new(path.as_ref())
+            .map_err(|_| FileError::BadFilePath)?;
+
+        unsafe {
+            let music = dos_like_sys::loadmod(filename.as_ptr() as *const _);
+            if music.is_null() {
+                return Err(FileError::FileNotFound);
+            }
+            Ok(Music(music))
+        }
+    }
+
+    /// Loads a music from a OPB file.
+    pub fn load_opb(path: impl AsRef<str>) -> Result<Music, FileError> {
+        let filename = CString::new(path.as_ref())
+            .map_err(|_| FileError::BadFilePath)?;
+
+        unsafe {
+            let music = dos_like_sys::loadopb(filename.as_ptr() as *const _);
+            if music.is_null() {
+                return Err(FileError::FileNotFound);
+            }
+            Ok(Music(music))
+        }
+    }
+
     /// Creates a music object from the byte data of a MUS file.
     pub fn create_mus(data: &[u8]) -> Music {
         // safety: although pointer type is *mut void_t,
@@ -60,13 +88,14 @@ impl Music {
         }
     }
     
-    /// Plays this music.
-    pub fn play(&self, loop_: bool, volume: u16) {
+    /// Plays this music,
+    /// stopping any other music currently playing.
+    /// 
+    /// If `loop_` is true, the music will loop forever.
+    /// `volume` is a number between 0 (silent) and 255 (full volume).
+    pub fn play(&self, loop_: bool, volume: u8) {
         unsafe {
             dos_like_sys::playmusic(self.0, loop_ as c_int, volume as c_int);
         }
     }
-    
 }
-
-
